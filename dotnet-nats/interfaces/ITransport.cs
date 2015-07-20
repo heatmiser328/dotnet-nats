7f3b9284e97520a751051f124315d6668564e161
Jeff Capuano <jcapuano328@gmail.com>
2015-07-20 07:20:44 -0400

    async tcp transport; not quite

---
+++ dotnet-nats/interfaces/ITransport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_nats
+{
+    public class TransportConnectionArgs : EventArgs
+    {
+        public TransportConnectionArgs(bool connected, bool reconnected) { Connected = connected; Reconnected = reconnected; }
+        public bool Connected {get; private set;}
+        public bool Reconnected {get; private set;}
+    }
+
+    public class TransportErrorArgs : EventArgs
+    {
+        public TransportErrorArgs(Exception ex) { Exception = ex; }
+        public Exception Exception { get; private set; }
+    }
+
+    public class TransportDataArgs : EventArgs
+    {
+        public TransportDataArgs(byte[] data, int size) { Data = data; Size = size; }
+        public byte[] Data { get; private set; }
+        public int Size { get; private set; }
+    }
+
+    public interface ITransport
+    {
+		event EventHandler<TransportConnectionArgs> Connected;
+		event EventHandler<TransportConnectionArgs> Reconnected;
+		event EventHandler<TransportConnectionArgs> Disconnected;
+		event EventHandler<TransportErrorArgs> Error;
+		event EventHandler<TransportDataArgs> Sent;
+        event EventHandler<TransportDataArgs> Received;
+		
+		Task<bool> Open(string address = null, int port = 0);
+        Task<bool> Close();
+        Task<bool> Send(string data);
+        Task<bool> Send(byte[] data);
+        Task<bool> Flush();
+    }
+}
---
+++ tests/mocks/MockTcpServer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace mocks
+{
+    internal class MockTcpServer
+    {
+        public const int cPort = 8877;
+        int _port = cPort;
+        TcpListener _listener;
+
+        public MockTcpServer(int port = cPort)
+        {
+            _port = port;
+            _listener = new TcpListener(IPAddress.Any, _port);
+            Reset();
+        }
+
+        public int Connections { get; private set; }
+        public IList<byte[]> Received { get; private set; }
+
+        public void Reset()
+        {
+            System.Diagnostics.Trace.WriteLine("MockTcpServer.Reset");
+            Connections = 0;
+            Received = new List<byte[]>();
+        }
+
+        public void Start()
+        {
+            System.Diagnostics.Trace.WriteLine("MockTcpServer.Start: Start listener");
+            _listener.Start();
+            Connect();            
+        }
+
+        public void Stop()
+        {
+            System.Diagnostics.Trace.WriteLine("MockTcpServer.Stop: Stop listener");
+            _listener.Stop();
+        }
+
+        private void Connect()
+        {
+            try
+            {
+                System.Diagnostics.Trace.WriteLine("MockTcpServer.Connect: Wait for socket connection");
+                _listener.BeginAcceptSocket((ar) => {
+                    TcpListener l = (TcpListener)ar.AsyncState;
+                    Socket s = l.EndAcceptSocket(ar);
+                    System.Diagnostics.Trace.WriteLine("MockTcpServer.Connect: Socket connected");
+                    Receive(new ReceiveState(s));
+                    Connect();
+                }, _listener);
+            }
+            catch{}
+        }
+
+        private class ReceiveState
+        {
+            public ReceiveState(Socket s) { socket = s; }
+            public Socket socket;
+            public const int BufferSize = 256;
+            public byte[] buffer = new byte[BufferSize];
+            public List<byte> message = new List<byte>();
+        }
+        private void Receive(ReceiveState st)
+        {
+            try
+            {
+                System.Diagnostics.Trace.WriteLine("MockTcpServer.Receive: Wait for data from Socket connection");
+                st.socket.BeginReceive(st.buffer, 0, ReceiveState.BufferSize, SocketFlags.None, (ar) => {
+                    ReceiveState state = (ReceiveState)ar.AsyncState;
+                    int received = state.socket.EndReceive(ar);
+                    System.Diagnostics.Trace.WriteLine(string.Format("MockTcpServer.Receive: Read {0} from Socket connection", received));
+                    if (received > 0)
+                    {
+                        state.message.AddRange(state.buffer.Take(received).ToList());
+                        Receive(state); // read the remaining
+                    }
+                    else
+                    {
+                        System.Diagnostics.Trace.WriteLine("MockTcpServer.Receive: Read message from Socket connection");
+                        Received.Add(state.message.ToArray());
+                        Receive(new ReceiveState(state.socket));
+                    }
+                }, st);
+            }
+            catch { }
+        }
+    }
+}
